56b1282441d70758404c0c89f76195449692a0d6
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-04-03 13:35:54 -0500

    tweak background color & bullet color. swap ship graphic

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -6,7 +6,7 @@ class Settings:
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 31, 84)
+        self.bg_color = (0, 16, 33)
 
         # Ship settings
         self.ship_speed = 1.5
@@ -15,4 +15,4 @@ class Settings:
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (80, 80, 80)
