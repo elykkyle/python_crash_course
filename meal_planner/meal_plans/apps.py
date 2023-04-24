f222ca9c459955e5469468e1d6c4a8938670afee
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-04-24 16:15:45 -0500

    correct app name to meal_plans

--- meal_planner/meal_plans/apps.py
+++ meal_planner/meal_plans/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MealPlannerConfig(AppConfig):
+class MealPlansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meal_planner'
+    name = 'meal_plans'
--- meal_planner/mp_project/settings.py
+++ meal_planner/mp_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "meal_planner",
+    "meal_plans",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
