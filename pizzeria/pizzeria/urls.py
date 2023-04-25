91178136c5523dce449641441018f183a9bfdaee
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-04-25 11:20:23 -0500

    create pizzas and pizza detail page

--- pizzeria/pizzeria/urls.py
+++ pizzeria/pizzeria/urls.py
@@ -7,5 +7,7 @@ from . import views
 app_name = "pizzeria"
 urlpatterns = [
     # Home page
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("pizzas", views.pizzas, name="pizzas"),
+    path("pizza/<int:pizza_id>", views.pizza, name="pizza"),
 ]
--- pizzeria/pizzeria/views.py
+++ pizzeria/pizzeria/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Pizza
 
 
 def index(request):
     """The home page for pizzeria."""
     return render(request, "pizzeria/index.html")
+
+
+def pizzas(request):
+    """Page to list all pizzas."""
+    pizzas = Pizza.objects.order_by("name")
+    context = {"pizzas": pizzas}
+    return render(request, "pizzeria/pizzas.html", context)
+
+
+def pizza(request, pizza_id):
+    """Detail page for single pizza"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by("name")
+    context = {"pizza": pizza, "toppings": toppings}
+    return render(request, "pizzeria/pizza.html", context)
