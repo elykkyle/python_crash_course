e2fb8d7c4e7f07a1b2f8e84a7fe8ccdd7af5e344
Kyle Williams <15004488+elykkyle@users.noreply.github.com>
2023-04-26 11:32:36 -0500

    create check topic owner func and check user before new entry

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -24,8 +24,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic.owner, request.user)
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
@@ -55,6 +54,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != "POST":
         # No data submitted; create a blank form.
@@ -78,8 +78,7 @@ def edit_entry(request, entry_id):
     """Edit existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != "POST":
         # Initial request; pre-fill form with the current entry.
@@ -93,3 +92,8 @@ def edit_entry(request, entry_id):
 
     context = {"entry": entry, "topic": topic, "form": form}
     return render(request, "learning_logs/edit_entry.html", context)
+
+
+def check_topic_owner(owner, user):
+    if owner != user:
+        raise Http404
